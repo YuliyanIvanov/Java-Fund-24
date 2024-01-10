74dfbea2f895d3167592f9ebaa1ccca538e512cb
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2024-01-10 19:08:04 +0200

    Update 1

---
+++ Java Fundamentals/Basic_Syntax_Conditional_Statements_And_Loops_Lab/A02_Passed.java
@@ -0,0 +1,17 @@
+package Basic_Syntax_Conditional_Statements_And_Loops_Lab;
+
+import java.util.Scanner;
+
+public class A02_Passed {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double grade= Double.parseDouble(scanner.nextLine());
+
+        if (grade >= 3.00){
+            System.out.println("Passed!");
+        } else {
+            System.out.println();
+        }
+    }
+}
