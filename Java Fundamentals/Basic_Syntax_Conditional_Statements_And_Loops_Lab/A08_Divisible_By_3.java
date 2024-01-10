a8888f5c8098cc397e80541ebc8d0cbba0d11527
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2024-01-10 19:54:20 +0200

    Update 8

---
+++ Java Fundamentals/Basic_Syntax_Conditional_Statements_And_Loops_Lab/A08_Divisible_By_3.java
@@ -0,0 +1,10 @@
+package Basic_Syntax_Conditional_Statements_And_Loops_Lab;
+
+public class A08_Divisible_By_3 {
+    public static void main(String[] args) {
+
+        for (int i = 3; i <= 100; i += 3) {
+            System.out.println(i);
+        }
+    }
+}
