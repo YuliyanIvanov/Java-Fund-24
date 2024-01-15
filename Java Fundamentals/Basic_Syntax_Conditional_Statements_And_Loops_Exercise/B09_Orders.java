dc748f2d98b28a7547977831ccdddcc905bbf972
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2024-01-15 07:19:38 +0200

    Update exercise

---
+++ Java Fundamentals/Basic_Syntax_Conditional_Statements_And_Loops_Exercise/B09_Orders.java
@@ -0,0 +1,26 @@
+package Basic_Syntax_Conditional_Statements_And_Loops_Exercise;
+
+import java.util.Scanner;
+
+public class B09_Orders {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int orders = Integer.parseInt(scanner.nextLine());
+
+        double money = 0;
+
+        for (int order = 1; order <= orders; order++) {
+
+            double pricePerCapsule = Double.parseDouble(scanner.nextLine());
+            int daysInMonth = Integer.parseInt(scanner.nextLine());
+            int capsulesCount = Integer.parseInt(scanner.nextLine());
+
+            double orderPrice = ((daysInMonth * capsulesCount) * pricePerCapsule);
+            money += orderPrice;
+
+            System.out.printf("The price for the coffee is: $%.2f\n", orderPrice);
+        }
+        System.out.printf("Total: $%.2f", money);
+    }
+}
