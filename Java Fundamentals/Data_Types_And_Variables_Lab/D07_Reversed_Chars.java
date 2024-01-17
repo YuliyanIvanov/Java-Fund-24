8a33fd28086f46dc0cfdb5eb585f52987123dec8
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2024-01-17 20:11:52 +0200

    Update Lab

---
+++ Java Fundamentals/Data_Types_And_Variables_Lab/D07_Reversed_Chars.java
@@ -0,0 +1,21 @@
+package Data_Types_And_Variables_Lab;
+
+import java.util.Scanner;
+
+public class D07_Reversed_Chars {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        char char1 = scanner.nextLine().charAt(0);
+        char char2 = scanner.nextLine().charAt(0);
+        char char3 = scanner.nextLine().charAt(0);
+
+        //String result = char3 + " " + char2 + " " + char1;
+        //System.out.println(result);
+
+        System.out.printf("%s %s %s", char3, char2, char1);
+
+        scanner.close();
+    }
+}
