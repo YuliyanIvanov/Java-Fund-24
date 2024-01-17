7d28780138fa7b56c345758efe738af1f04f8533
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2024-01-17 18:42:27 +0200

    Update Lab

---
+++ Java Fundamentals/Data_Types_And_Variables_Lab/D01_Convert_Meters_To_Kilometers.java
@@ -0,0 +1,15 @@
+package Data_Types_And_Variables_Lab;
+
+import java.util.Scanner;
+
+public class D01_Convert_Meters_To_Kilometers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int meters = Integer.parseInt(scanner.nextLine());
+
+        double kilometers = meters / 1000.0;
+
+        System.out.printf("%.2f", kilometers);
+    }
+}
