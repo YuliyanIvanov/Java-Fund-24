a2c37a280b38fe3f8e67bce6bbc80eebb1f9527b
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2024-01-19 18:57:04 +0200

    Update Exercise

---
+++ Java Fundamentals/Data_Types_And_Variables_Exercise/E03_Elevator.java
@@ -0,0 +1,22 @@
+package Data_Types_And_Variables_Exercise;
+
+import java.util.Scanner;
+
+public class E03_Elevator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int people = Integer.parseInt(scanner.nextLine());
+        int capacity = Integer.parseInt(scanner.nextLine());
+
+        if (people <= capacity) {
+            System.out.println(1);
+        } else if (people % capacity == 0) {
+            int a = people / capacity;
+            System.out.println(a);
+        } else if (people % capacity != 0) {
+            int b = people / capacity;
+            System.out.println(b + 1);
+        }
+    }
+}
