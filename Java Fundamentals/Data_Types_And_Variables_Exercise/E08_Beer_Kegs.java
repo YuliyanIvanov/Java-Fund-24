89954f5a1ae4bd835dc708410c925b73656c3c5a
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2024-01-19 19:53:52 +0200

    Update Exercise

---
+++ Java Fundamentals/Data_Types_And_Variables_Exercise/E08_Beer_Kegs.java
@@ -0,0 +1,29 @@
+package Data_Types_And_Variables_Exercise;
+
+import java.util.Scanner;
+
+public class E08_Beer_Kegs {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numberOfKegs = Integer.parseInt(scanner.nextLine());
+
+        double maxVolume = Double.MIN_VALUE;
+        String biggestModelKeg = "";
+
+        for (int keg = 1; keg <= numberOfKegs; keg++) {
+            String model = scanner.nextLine();
+
+            double radius = Double.parseDouble(scanner.nextLine());
+            int height = Integer.parseInt(scanner.nextLine());
+
+            double volume = Math.PI * (radius * radius) * height;
+
+            if (volume > maxVolume) {
+                maxVolume = volume;
+                biggestModelKeg = model;
+            }
+        }
+        System.out.println(biggestModelKeg);
+    }
+}
