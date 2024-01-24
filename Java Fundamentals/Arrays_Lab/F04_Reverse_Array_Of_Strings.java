1e6f541dd4d4fc4d63a37f0c59834b3c118028f1
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2024-01-24 20:53:07 +0200

    Update Lab

---
+++ Java Fundamentals/Arrays_Lab/F04_Reverse_Array_Of_Strings.java
@@ -0,0 +1,29 @@
+package Arrays_Lab;
+
+import java.util.Scanner;
+
+public class F04_Reverse_Array_Of_Strings {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] inputArray = scanner.nextLine().split(" ");
+
+        reverseArray(inputArray);
+
+        System.out.println(String.join(" ", inputArray));
+    }
+
+    private static void reverseArray(String[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            String temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+}
