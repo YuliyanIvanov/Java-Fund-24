67cf16f834413fd466fdc4ccd3af7cb889e1b04a
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2024-01-29 19:58:13 +0200

    Update exercise

---
+++ Java Fundamentals/Arrays_Exercise/D03_Zig_Zag_Arrays.java
@@ -0,0 +1,11 @@
+package Arrays_Exercise;
+
+import java.util.Scanner;
+
+public class D03_Zig_Zag_Arrays {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
