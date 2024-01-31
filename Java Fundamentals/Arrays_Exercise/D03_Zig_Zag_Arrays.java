0162723f18f11b4d241bd42f62db5dc2aa4c3ad1
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2024-01-31 18:58:51 +0200

    Update exercise

--- Java Fundamentals/Arrays_Exercise/D03_Zig_Zag_Arrays.java
+++ Java Fundamentals/Arrays_Exercise/D03_Zig_Zag_Arrays.java
@@ -6,6 +6,46 @@ public class D03_Zig_Zag_Arrays {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int n = Integer.parseInt(scanner.nextLine());
 
+        String[] firstArray = new String[n];
+        String[] secondArray = new String[n];
+
+        createArrays(scanner, n, firstArray, secondArray);
+
+        printArray(firstArray);
+
+        printArray(secondArray);
+
+        scanner.close();
+    }
+
+    private static void createArrays(Scanner scanner, int n, String[] firstArray, String[] secondArray) {
+        int position = 0;
+
+        for (int row = 1; row <= n; row++) {
+            String textRow = scanner.nextLine();
+            String[] currentRowElements = textRow.split(" ");
+
+            String firstElement = currentRowElements[0];
+            String secondElement = currentRowElements[1];
+
+            if (row % 2 != 0) {
+                firstArray[position] = firstElement;
+                secondArray[position] = secondElement;
+            } else {
+                firstArray[position] = secondElement;
+                secondArray[position] = firstElement;
+            }
+
+            position++;
+        }
+    }
+
+    private static void printArray(String[] arr) {
+        for (String el : arr) {
+            System.out.print(el + " ");
+        }
+        System.out.println();
     }
 }
