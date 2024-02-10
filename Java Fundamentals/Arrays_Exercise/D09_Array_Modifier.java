0fd7e6e29653714e1864929491fc74671d25a0f9
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2024-02-10 14:04:02 +0200

    Update exercise

---
+++ Java Fundamentals/Arrays_Exercise/D09_Array_Modifier.java
@@ -0,0 +1,51 @@
+package Arrays_Exercise;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class D09_Array_Modifier {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] numbers = Arrays.stream(scanner.nextLine()
+                        .split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        String command = scanner.nextLine();
+        while (!command.equals("end")) {
+
+            if (command.contains("swap")) {
+                int firstIndex = Integer.parseInt(command.split(" ")[1]);
+                int secondIndex = Integer.parseInt(command.split(" ")[2]);
+
+                int firstIndexNumber = numbers[firstIndex];
+                int secondIndexNumber = numbers[secondIndex];
+
+                numbers[firstIndex] = secondIndexNumber;
+                numbers[secondIndex] = firstIndexNumber;
+            } else if (command.contains("multiply")) {
+                int firstIndex = Integer.parseInt(command.split(" ")[1]);
+                int secondIndex = Integer.parseInt(command.split(" ")[2]);
+
+                int firstIndexNumber = numbers[firstIndex];
+                int secondIndexNumber = numbers[secondIndex];
+
+                int product = firstIndexNumber * secondIndexNumber;
+
+                numbers[firstIndex] = product;
+
+            } else {
+                for (int position = 0; position <= numbers.length - 1; position++) {
+                    numbers[position]--;
+                }
+            }
+            command = scanner.nextLine();
+        }
+        System.out.println(
+                Arrays.toString(numbers)
+                        .replace("[", "")
+                        .replace("]", "")
+        );
+    }
+}
