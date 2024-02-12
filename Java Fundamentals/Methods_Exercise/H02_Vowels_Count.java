68baf86c10e90d19a7dcbcec4df93cf7f1521cb5
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2024-02-12 13:27:25 +0200

    Update lab

---
+++ Java Fundamentals/Methods_Exercise/H02_Vowels_Count.java
@@ -0,0 +1,52 @@
+package Methods_Exercise;
+
+import java.util.Scanner;
+
+public class H02_Vowels_Count {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String text = scanner.nextLine();
+
+        int count = vowelsCount(text);
+
+        System.out.println(count);
+    }
+
+    public static int vowelsCount(String text) {
+        int count = 0;
+
+        text = text.toLowerCase();
+
+        for (int i = 0; i < text.length(); i++) {
+            char symbol = text.charAt(i);
+            switch (symbol){
+                case 'a':
+                    count++;
+                    break;
+                case 'e':
+                    count++;
+                    break;
+                case 'i':
+                    count++;
+                    break;
+                case 'o':
+                    count++;
+                    break;
+                case 'u':
+                    count++;
+                    break;
+                default:
+                    break;
+            }
+            /*if (symbol == 'a' ||
+                    symbol == 'e' ||
+                    symbol == 'i' ||
+                    symbol == 'o' ||
+                    symbol == 'u') {
+                count++;
+            }*/
+        }
+        return count;
+    }
+}
