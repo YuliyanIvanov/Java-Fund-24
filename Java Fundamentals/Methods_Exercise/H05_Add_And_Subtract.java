de9621461e92a34f73bacef3c7eeb743523c0fa1
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2024-02-17 13:35:39 +0200

    Update exercise

---
+++ Java Fundamentals/Methods_Exercise/H05_Add_And_Subtract.java
@@ -0,0 +1,26 @@
+package Methods_Exercise;
+
+import java.util.Scanner;
+
+public class H05_Add_And_Subtract {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int firstNumber = Integer.parseInt(scanner.nextLine());
+        int secondNumber = Integer.parseInt(scanner.nextLine());
+        int thirdNumber = Integer.parseInt(scanner.nextLine());
+
+        int sum = sumOfTwoNumbers(firstNumber, secondNumber);
+        int result = subtractTwoNumbers(sum, thirdNumber);
+
+        System.out.println(result);
+    }
+
+    public static int sumOfTwoNumbers(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+    public static int subtractTwoNumbers(int firstNumber, int secondNumber) {
+        return firstNumber - secondNumber;
+    }
+}
