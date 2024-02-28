1388a0fe05418e0486d0b3b7a3acff7a6c1199ad
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2024-02-28 20:14:44 +0200

    Update lab

---
+++ Java Fundamentals/Maps_Lambda_And_Stream_API_Lab/K01_Count_Real_Numbers.java
@@ -0,0 +1,24 @@
+package Maps_Lambda_And_Stream_API_Lab;
+
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class K01_Count_Real_Numbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        String[] numbers = input.split(" ");
+
+        TreeMap<Double, Integer> counts = new TreeMap<>();
+
+        for (String number : numbers) {
+            double num = Double.parseDouble(number);
+            counts.put(num, counts.getOrDefault(num, 0) + 1);
+        }
+
+        for (double num : counts.keySet()) {
+            System.out.printf("%.0f -> %d%n", num, counts.get(num));
+        }
+    }
+}
