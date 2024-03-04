36bfe66fc7cdbee3eb7b748497bbc2bdbe6ea159
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2024-03-04 14:00:49 +0200

    Update lab

--- Java Fundamentals/Demo.java
+++ Java Fundamentals/Demo.java
@@ -4,18 +4,10 @@ public class Demo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String input = scanner.nextLine();
-        String[] numbers = input.split(" ");
+        int[] array = new int[10];
+        List<Integer> list = new ArrayList<>();
 
-        TreeMap<Double, Integer> counts = new TreeMap<>();
-
-        for (String number : numbers) {
-            double num = Double.parseDouble(number);
-            counts.put(num, counts.getOrDefault(num, 0) + 1);
-        }
-
-        for (double num : counts.keySet()) {
-            System.out.printf("%.0f -> %d%n", num, counts.get(num));
-        }
+        System.out.println(array.length);
+        System.out.println(list.size());
     }
 }
--- Java Fundamentals/Lists_Lab/I01_List_Manipulation_Basics.java
+++ Java Fundamentals/Lists_Lab/I01_List_Manipulation_Basics.java
@@ -1,11 +1,53 @@
 package Lists_Lab;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class I01_List_Manipulation_Basics {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        List<Integer> numbers = Arrays
+                .stream(scanner.nextLine()
+                        .split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
 
+        while (true) {
+            String line = scanner.nextLine();
+
+
+            if (line.equals("end")) {
+                break;
+            }
+            String[] tokens = line.split(" ");
+
+            switch (tokens[0]) {
+                case "Add":
+                    int numToAdd = Integer.parseInt(tokens[1]);
+                    numbers.add(numToAdd);
+                    break;
+                case "Remove":
+                    int numToRemove = Integer.parseInt(tokens[1]);
+                    numbers.remove(Integer.valueOf(numToRemove));
+                    break;
+                case "RemoveAt":
+                    int indexToRemove = Integer.parseInt(tokens[1]);
+                    numbers.remove(indexToRemove);
+                    break;
+                case "Insert":
+                    int numToInsert = Integer.parseInt(tokens[1]);
+                    int insertIndex = Integer.parseInt(tokens[2]);
+                    numbers.add(insertIndex, numToInsert);
+                    break;
+                default:
+                    break;
+            }
+        }
+        System.out.println(numbers
+                .toString()
+                .replaceAll("[\\[\\],]", ""));
     }
 }
