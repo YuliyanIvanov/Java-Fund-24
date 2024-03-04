21fa4956b8cf7f0bb805519a234586235877996e
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2024-03-04 12:34:31 +0200

    Update exercise

---
+++ Java Fundamentals/Maps_LambdaA_nd_Stream_API_Exercise/L08_Company_Users.java
@@ -0,0 +1,32 @@
+package Maps_LambdaA_nd_Stream_API_Exercise;
+
+import java.util.*;
+
+public class L08_Company_Users {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Map<String, List<String>> listOfEmployees = new LinkedHashMap<>();
+
+        String input = scanner.nextLine();
+        while (!input.equals("End")) {
+
+            String[] data = input.split(" -> ");
+
+            List<String> currentCompanyEmployees = listOfEmployees.get(data[0]);
+            boolean containsId = currentCompanyEmployees != null && currentCompanyEmployees.contains(data[1]);
+
+            if (!containsId) {
+                listOfEmployees.putIfAbsent(data[0], new ArrayList<>());
+                listOfEmployees.get(data[0]).add(data[1]);
+            }
+
+            input = scanner.nextLine();
+        }
+
+        for (Map.Entry<String, List<String>> entry : listOfEmployees.entrySet()) {
+            System.out.printf("%s\n", entry.getKey());
+            entry.getValue().forEach(id -> System.out.printf("-- %s\n", id));
+        }
+    }
+}
