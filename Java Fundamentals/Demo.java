f4c37426595c0b177d8429d4280810be9c27b53e
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2024-03-23 13:42:42 +0200

    Update Lab

--- Java Fundamentals/Demo.java
+++ Java Fundamentals/Demo.java
@@ -4,6 +4,20 @@ public class Demo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        
+        Random random = new Random();
+
+        System.out.println("Enter 'start' to generate a random number or 'exit' to quit:");
+
+        String command;
+        while (!(command = scanner.nextLine()).equals("exit")) {
+            if (command.equals("start")) {
+                int randomNumber = random.nextInt(101);
+                System.out.println("Random number: " + randomNumber);
+            } else {
+                System.out.println("Unknown command. Please enter 'start' or 'exit'.");
+            }
+        }
+
+        System.out.println("Exiting the random number generator.");
     }
 }
