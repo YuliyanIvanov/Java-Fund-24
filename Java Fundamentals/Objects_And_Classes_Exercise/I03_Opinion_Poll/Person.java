f69c355b801f78f5a8e22f6daa32e8e269e6caa6
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2024-03-30 14:12:45 +0200

    Update Lab

--- Java Fundamentals/Objects_And_Classes_Exercise/I03_Opinion_Poll/Person.java
+++ Java Fundamentals/Objects_And_Classes_Exercise/I03_Opinion_Poll/Person.java
@@ -1,8 +1,8 @@
 package Objects_And_Classes_Exercise.I03_Opinion_Poll;
 
 public class Person {
-    private String name;
-    private int age;
+    private final String name;
+    private final int age;
 
     public Person(String name, int age) {
         this.name = name;
